f25359344e7c257dc2c870d86f14fcde450505e4
seyda <iclalseyda@gmail.com>
2022-04-13 11:36:01 +0200

    day38_Inheritance continue

--- src/day38_Inheritance/Car.java
+++ src/day38_Inheritance/Car.java
@@ -1,5 +1,40 @@
 package day38_Inheritance;
 
+import java.security.PublicKey;
+
 public class Car {
 
+    public String brand,model;
+    public int year;
+    public double price;
+    public String color;
+    public int miles;
+
+    public Car(String brand, String model, int year, double price, String color, int miles) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.miles = miles;
+    }
+
+    public void start(){
+        System.out.print(brand + " "+model + "is starting");
+    }
+    public void drive(){
+        System.out.print(brand+ " "+ model+ "is driving");
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", miles=" + miles +
+                '}';
+    }
 }
